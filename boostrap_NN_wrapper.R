f14e1f33cddb8ea6f6e25b0515e11c8ecbc3f632
Chun-Chen Tu <timtu@umich.edu>
2018-10-17 19:48:58 -0400

    update for bootstrap wrapper

--- boostrap_NN_wrapper.R
+++ boostrap_NN_wrapper.R
@@ -3,3 +3,5 @@ load_all(".")
 source("boostrap_NN_fun.R")
 args = (commandArgs(TRUE))
 eval(parse(text=args[[1]])) # cvID
+eval(parse(text=args[[2]])) # save_prefix
+boostrap_NN_fun(cvID, save_prefix)
